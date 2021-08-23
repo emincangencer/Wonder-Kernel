Divyanshu-Modi/Atom-X-Kernel

zram_drv: allow overriding zram size from kernel
Add a CONFIG_ZRAM_SIZE_OVERRIDE option, size will be set
to 1 GB multiply by CONFIG_ZRAM_SIZE_OVERRIDE.

My[Co-author] edits :
1) Set range to 1-4 gb (This is a android kernel intended for sdm660 platform so....... we don't need 8 gb of zram)
2) Set default zram to 1gb (Better performance & battery balance is achieved)
3) Fix the whole feking commit (Previously this commit made changes which would override zram when the override flag was disable and it would parse roms set figure when it was enabled literally it was doing the opposite of the commits meaning so.... I fixed it)

Co-authored-by: Divyanshu-Modi <divyan.m05@gmail.com>
Signed-off-by: Dmitry Eliseev <dmitry.e@cloudberrylab.com>
Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
Signed-off-by: sohamxda7 <sensoham135@gmail.com>
(cherry picked from commit 0a2963189676acee5dd859f483448399b0dee1e2)
(cherry picked from commit f9309d10ced2e390ecacabfee926e3d9e868ef4a)

drivers/block/zram/zram_drv.c
@@ -1497,9 +1497,14 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
+#ifndef CONFIG_ZRAM_SIZE_OVERRIDE
+	disksize = (u64)SZ_1G * CONFIG_ZRAM_SIZE_OVERRIDE;
+	pr_info("Overriding zram size to %li", disksize);
+#else
 	disksize = memparse(buf, NULL);
 	if (!disksize)
 		return -EINVAL;
+#endif
 
 	down_write(&zram->init_lock);
 	if (init_done(zram)) {

emincangencer/Wonder-Kernel

Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

drivers/block/zram/zram_drv.c
@@ -1497,9 +1497,14 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
+#ifdef CONFIG_ZRAM_SIZE_OVERRIDE
+	disksize = (u64)SZ_1G * CONFIG_ZRAM_SIZE_OVERRIDE;
+	pr_info("Overriding zram size to %li", disksize);
+#else
 	disksize = memparse(buf, NULL);
 	if (!disksize)
 		return -EINVAL;
+#endif
 
 	down_write(&zram->init_lock);
 	if (init_done(zram)) {